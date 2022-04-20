424675d72d56734b0a528b705c5a2d6c83be072f
HAWAINA <ismettrabzon08@gmail.com>
2022-04-20 22:27:40 +0600

    first commit

--- book/urls.py
+++ book/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('hello/', views.hello_world, name="hello"),
     path('books/', views.book_all, name="book_All"),
     path("books/<int:id>/", views.book_detail, name="book_detail"),
 ]
